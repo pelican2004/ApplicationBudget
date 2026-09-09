bf24eb2c822a7ca8484f44b5ed002b41b4517e49
pelican2004 <dmarismiron@gmail.com>
2026-09-09 10:54:04 +0300

    Refactor server/app.js for improved readability

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes=require("./routes/expenseRoutes");
@@ -9,18 +8,15 @@ const savingRoutes = require("./routes/savingRoutes");
 const savingTransactionRoutes = require("./routes/savingTransactionRoutes");
 const billRoutes = require("./routes/billRoutes");
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
 app.get("/", (req, res) => {
   res.json({
     success: true,
     message: "Application Budget API funcționează!",
   });
 });
-
 app.use("/api/auth", authRoutes);
 app.use("/api/income", incomeRoutes);
 app.use("/api/expense", expenseRoutes);
