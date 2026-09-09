2f05a4d2e3983618cc3ea7bc59357967575010ed
pelican2004 <dmarismiron@gmail.com>
2026-09-09 10:52:47 +0300

    Refactor savingTransactionRoutes.js for cleaner code

--- server/routes/savingTransactionRoutes.js
+++ server/routes/savingTransactionRoutes.js
@@ -1,33 +1,26 @@
 const express = require("express");
-
 const router = express.Router();
-
 const {
   createTransaction,
   getTransactions,
   deleteTransaction,
 } = require("../controllers/savingTransactionController");
-
 const authMiddleware = require(
   "../middleware/authMiddleware"
 );
-
 router.post(
   "/",
   authMiddleware,
   createTransaction
 );
-
 router.get(
   "/:saving",
   authMiddleware,
   getTransactions
 );
-
 router.delete(
   "/:id",
   authMiddleware,
   deleteTransaction
 );
-
 module.exports = router;
