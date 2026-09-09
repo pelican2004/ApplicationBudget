cb2d27f9bd8f4ecb3db1c1218a9539258d7f9e6f
pelican2004 <dmarismiron@gmail.com>
2026-09-09 10:49:07 +0300

    Update Expense.js

--- server/models/Expense.js
+++ server/models/Expense.js
@@ -7,12 +7,10 @@ const expenseSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-
     amount: {
       type: Number,
       required: [true, "Suma este obligatorie"],
     },
-
     category: {
       type: String,
       required: [true, "Categoria este obligatorie"],
@@ -21,25 +19,19 @@ const expenseSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
-
     description: {
       type: String,
       default: "",
     },
-
     date: {
       type: Date,
       default: Date.now,
     },
-
-    // Depunerea de economii care a generat această cheltuială
     savingTransaction: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "SavingTransaction",
       default: null,
     },
-
-    // Factura care a generat această cheltuială
     bill: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Bill",
