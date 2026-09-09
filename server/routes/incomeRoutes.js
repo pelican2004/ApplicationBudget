50e43450a3377e058664bca0cac4ad30b85be755
pelican2004 <dmarismiron@gmail.com>
2026-09-09 10:51:58 +0300

    Refactor incomeRoutes.js for cleaner code
    Removed unnecessary whitespace and comments for cleaner code.

--- server/routes/incomeRoutes.js
+++ server/routes/incomeRoutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const router = express.Router();
-
 const {
   createIncome,
   getIncomes,
@@ -9,24 +7,19 @@ const {
 } = require("../controllers/incomeController");
 
 const authMiddleware = require("../middleware/authMiddleware");
-
-// Toate rutele de venituri necesită autentificare
 router.post(
   "/",
   authMiddleware,
   createIncome
 );
-
 router.get(
   "/:user",
   authMiddleware,
   getIncomes
 );
-
 router.delete(
   "/:id",
   authMiddleware,
   deleteIncome
 );
-
 module.exports = router;
